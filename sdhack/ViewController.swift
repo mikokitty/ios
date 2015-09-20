c65c01dbc7704e22193747d2afe030fd1254b5f9
mikokitty <jlxie@ucsd.edu>
2015-09-20 15:59:27 -0700

    Initial Commit

---
+++ sdhack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sdhack
+//
+//  Created by Scarlet Xie on 9/20/15.
+//  Copyright (c) 2015 Scarlet Xie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
